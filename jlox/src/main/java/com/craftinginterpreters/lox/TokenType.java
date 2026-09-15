4aad84a35922a11f540c1ccceb0d1c5ee5a90b34
mdipanjan <mdipanjan33@gmail.com>
2026-09-15 15:27:18 +0530

    chore: lox - Scanner class for literals, operators

--- jlox/src/main/java/com/craftinginterpreters/lox/TokenType.java
+++ jlox/src/main/java/com/craftinginterpreters/lox/TokenType.java
@@ -6,13 +6,7 @@ public enum TokenType {
     LEFT_BRACE, RIGHT_BRACE,
     COMMA, DOT, MINUS, PLUS, SEMICOLON, COLON,
     QUESTION_MARK,
-    STAR,
-    BANG, BANG_EQUAL,
-    EQUAL, EQUAL_EQUAL,
-    GREATER, GREATER_EQUAL,
-    LESS, LESS_EQUAL,
-
-    // One or two character tokens.
+    STAR, SLASH,
     BANG, BANG_EQUAL,
     EQUAL, EQUAL_EQUAL,
     GREATER, GREATER_EQUAL,
